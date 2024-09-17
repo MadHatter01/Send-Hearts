68b2dfdf37e64e6e39eb312406ad1418a7ba9026
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-09-17 06:51:58 -0700

    Monitor server status and fallback message

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,51 @@ import './App.css'
 import Emojis from './components/Emojis'
 import ReactionCounter from './components/ReactionCounter';
 
-const socket = io('http://localhost:3001');
+const SERVER_URL = import.meta.env.VITE_API_URL
+const socket = io(SERVER_URL);
 
 function App() {
+  const [serverOnline, setServerOnline] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const [reactions, setReactions] = useState({});
-const sendReaction = (emoji)=>{
-  socket.emit("send_reactions", emoji);
-}
+  const sendReaction = (emoji) => {
+    socket.emit("send_reactions", emoji);
+  }
 
-useEffect (()=>{
-  
-  socket.on("reaction_display", (data)=>{
-    console.log('receiving data from server')
-    setReactions(data);
-  });
-  
+  useEffect(() => {
 
-}, []);
+    socket.on('connect_error', ()=>{
+      setServerOnline(false);
+      setErrorMessage('Unable to connect to the server. Please try again later');
+    })
+
+    socket.on('connect', () => {
+      setServerOnline(true);
+      setErrorMessage('');
+    });
+    socket.on("reaction_display", (data) => {
+      console.log('receiving data from server')
+      setReactions(data);
+    });
+
+    return () => {
+      socket.off('connect_error');
+
+      socket.off('reaction_display');
+    }
+
+
+  }, []);
+
+  if (!serverOnline) {
+    return <div className='error-message'>{errorMessage}</div>;
+  }
 
   return (
     <div className='app-container'>
-      <Emojis sendReaction={sendReaction}/>
-      <ReactionCounter reactions = {reactions}/>
+      <Emojis sendReaction={sendReaction} />
+      <ReactionCounter reactions={reactions} />
     </div>
   )
 }
